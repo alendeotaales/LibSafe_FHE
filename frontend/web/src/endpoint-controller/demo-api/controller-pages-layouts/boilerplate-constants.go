53e1984333f69e13c763b6cf0e0f54f6fb58aa94
alendeotaales <alendeotaales@users.noreply.github.com>
2025-11-16 11:46:08 +0000

    Add boilerplate-constants.go, global-service.adoc, dist.java

---
+++ frontend/web/src/endpoint-controller/demo-api/controller-pages-layouts/boilerplate-constants.go
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/libsafe_fhe/frontend/web/src/endpoint-controller/demo-api/controller-pages-layouts/boilerplate-constants.go
+// Generated at: 2025-12-03T23:45:46.670Z
+
