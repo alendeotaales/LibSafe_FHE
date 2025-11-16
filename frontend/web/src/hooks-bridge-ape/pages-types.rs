5618196ad8fb121c360201fcf2cdc0423daa31a1
alendeotaales <alendeotaales@users.noreply.github.com>
2025-11-16 14:25:27 +0000

    Add pages-types.rs, util.ts, main.tsx

---
+++ frontend/web/src/hooks-bridge-ape/pages-types.rs
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/libsafe_fhe/frontend/web/src/hooks-bridge-ape/pages-types.rs
+// Generated at: 2025-12-03T23:45:46.671Z
+
---
+++ frontend/web/src/hooks-bridge-ape/util.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+export const hooks_manager = () => { return null; };
+
